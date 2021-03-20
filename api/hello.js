dc1559fb1f68d08db4795c710d92d19c15f2e612
Humayun Javed <arnetraff@gmail.com>
2021-03-21 00:02:01 +0500

    Firestore config

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,22 @@
+import * as admin from 'firebase-admin';
+
+var serviceAccount = require("../service-key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+const db = admin.firestore();
+
+
 module.exports = (req,res) => {
     const {name = "World"}  = req.query;
+    
+    const docRef = db.collection('users').doc('alovelace');
+    docRef.set({
+        first:'Ada',
+        last:  'Lovelace',
+        born:1815
+    });
     res.status(200).send(`Hello ${name}!`);
 }
